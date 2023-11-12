08f3f922a2a53b41be868d872d20e7f50a304e72
sun4red <klzxcv1@gmail.com>
2023-11-12 11:19:59 +0900

    spring setting

--- acc_spring/src/main/java/acc/controller/TestController.java
+++ acc_spring/src/main/java/acc/controller/TestController.java
@@ -1,17 +1,22 @@
 package acc.controller;
 
+import org.apache.ibatis.io.ResolverUtil.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
+	private static final Logger logger = LoggerFactory.getLogger(Test.class);
 	
-	@RequestMapping("tomain")	
+	@RequestMapping("/")	
 	public String toMain() {
 		
+	logger.info("testing");
 		
-		return "boardForm";
+		return "board/boardForm";
 	}
 	
 }
--- acc_spring/src/main/webapp/WEB-INF/views/index.jsp
+++ acc_spring/src/main/webapp/WEB-INF/views/index.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 <script type = "text/javascript">
-location.href="tomain";
+location.href="/";
 </script>
 </body>
 </html>
